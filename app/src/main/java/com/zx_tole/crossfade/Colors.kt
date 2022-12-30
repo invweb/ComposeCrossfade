d42c94a89a5dd23feb135cca89b0fc3106be4742
Vasily <invweb@gmail.com>
2022-12-30 15:32:57 +0300

    Initial commit

---
+++ app/src/main/java/com/zx_tole/crossfade/Colors.kt
@@ -0,0 +1,7 @@
+package com.zx_tole.crossfade
+
+import androidx.compose.ui.graphics.Color
+
+enum class Colors(val color: Color) {
+    Red(Color.Red), Green(Color.Green), Blue(Color.Blue)
+}
---
+++ app/src/main/java/com/zx_tole/crossfade/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.zx_tole.crossfade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.zx_tole.crossfade.ui.theme.CrossfadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CrossfadeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CrossfadeDemo()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CrossfadeDemo() {
+    var currentColor by remember { mutableStateOf(Colors.Red) }
+    Column {
+        Row {
+            Colors.values().forEach { myColors ->
+                Button(
+                    onClick = { currentColor = myColors },
+                    Modifier
+                        .weight(1f, true)
+                        .height(48.dp)
+                        .background(myColors.color),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = myColors.color)
+                ) {
+                    Text(myColors.name)
+                }
+            }
+        }
+        Crossfade(targetState = currentColor, animationSpec = tween(3000)) { selectedColor ->
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(selectedColor.color))
+        }
+    }
+}
